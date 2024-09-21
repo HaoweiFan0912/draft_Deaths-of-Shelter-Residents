680e533d29611de026a60bc1b7698c8fb4a58d88
HaoweiFan0912 <fhw0912@gmail.com>
2024-09-21 19:53:09 -0400

    Downloading the dataset
    Data has been downloaded as csv.

--- scripts/01-download_data.R
+++ scripts/01-download_data.R
@@ -1,26 +1,34 @@
 #### Preamble ####
-# Purpose: Downloads and saves the data from [...UPDATE THIS...]
-# Author: Rohan Alexander [...UPDATE THIS...]
-# Date: 11 February 2023 [...UPDATE THIS...]
-# Contact: rohan.alexander@utoronto.ca [...UPDATE THIS...]
+# Purpose: Downloads and saves the data from "opendatatoronto"
+# Author: Haowei Fan
+# Date: 21 September 2024
+# Contact: haowei.fan@mail.utoronto.ca
 # License: MIT
-# Pre-requisites: [...UPDATE THIS...]
-# Any other information needed? [...UPDATE THIS...]
+# Pre-requisites: None
+# Any other information needed? None
 
 
 #### Workspace setup ####
 library(opendatatoronto)
 library(tidyverse)
-# [...UPDATE THIS...]
 
 #### Download data ####
-# [...ADD CODE HERE TO DOWNLOAD...]
 
+#Looking at first 200 pages of datasets from "opendatatoronto".
+list_packages <- list_packages(200)
 
+#Searching for the dataset called "Deaths of Shelter Residents" which I am interested in.
+list_deathShelter <- search_packages("Deaths of Shelter Residents")
+
+#Get the id of the dataset.
+list_deathShelter_resources <- list_deathShelter %>%
+  list_package_resources()
+
+#Download the data by its' id
+deaths_of_shelter_residents <- list_deathShelter_resources[3,"id"] %>%
+  get_resource()
 
 #### Save data ####
-# [...UPDATE THIS...]
-# change the_raw_data to whatever name you assigned when you downloaded it.
-write_csv(the_raw_data, "inputs/data/raw_data.csv") 
+write_csv(deaths_of_shelter_residents, "data/raw_data/raw_data.csv") 
 
          
