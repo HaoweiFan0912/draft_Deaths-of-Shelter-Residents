e449702e5234aefe52b18ca7931d96503a63289c
HaoweiFan0912 <fhw0912@gmail.com>
2024-09-21 19:15:18 -0400

    Simulation and some tests
    Simulation of data. And some tests for checking whether datas are in the formation that I want.

--- scripts/00-simulate_data.R
+++ scripts/00-simulate_data.R
@@ -1,19 +1,41 @@
 #### Preamble ####
-# Purpose: Simulates... [...UPDATE THIS...]
-# Author: Rohan Alexander [...UPDATE THIS...]
-# Date: 11 February 2023 [...UPDATE THIS...]
-# Contact: rohan.alexander@utoronto.ca [...UPDATE THIS...]
+# Purpose: Simulates data
+# Author: Haowei Fan
+# Date: 21 September 2024
+# Contact: haowei.fan@mail.utoronto.ca
 # License: MIT
-# Pre-requisites: [...UPDATE THIS...]
-# Any other information needed? [...UPDATE THIS...]
+# Pre-requisites: None
+# Any other information needed? None
 
 
 #### Workspace setup ####
 library(tidyverse)
-# [...UPDATE THIS...]
 
 #### Simulate data ####
-# [...ADD CODE HERE...]
+
+#Set seed for reproducibility.
+set.seed(902)
+
+#The start year is 2007, and the end year is 2024.
+years <- seq(2007, 2024)
+
+#There are four seasons in a year.
+seasons <- rep(c("spring", "summer", "autumn", "winter"))
+
+#I assume the number of decedents follows poisson distribution with mean of 8.
+total_decedents <- rpois(length(years)*4, 8)
+
+#Begin simulation.
+simulated_data <-
+  tibble(
+    year = rep(years, each = 4),
+    season = rep(seasons, times = length(years)),
+    total_decedents = total_decedents
+  )
+simulated_data
+
+
+
 
 
 
--- scripts/03-test_data.R
+++ scripts/03-test_data.R
@@ -1,15 +1,34 @@
 #### Preamble ####
-# Purpose: Tests... [...UPDATE THIS...]
-# Author: Rohan Alexander [...UPDATE THIS...]
-# Date: 11 February 2023 [...UPDATE THIS...]
-# Contact: rohan.alexander@utoronto.ca [...UPDATE THIS...]
+# Purpose: Tests codes
+# Author: Haowei Fan
+# Date: 21 September 2024
+# Contact: haowei.fan@mail.utoronto.ca
 # License: MIT
-# Pre-requisites: [...UPDATE THIS...]
-# Any other information needed? [...UPDATE THIS...]
+# Pre-requisites: None
+# Any other information needed? None
 
 
 #### Workspace setup ####
 library(tidyverse)
-# [...UPDATE THIS...]
 
 #### Test data ####
+
+#Import data.
+data <- simulated_data
+
+#"years" is an integer
+all(data$year == floor(data$year))
+
+#"years" is in the interval (2007,2024).
+all(data$year <= 2024 & data$year >= 2007)
+
+#"season" is one of "spring", "autumn", "summer", "winter".
+all(data$season %in% c("spring", "autumn", "summer", "winter"))
+
+#"total decedents" in an integer.
+all(data$total_decedents == floor(data$total_decedents))
+
+#"total decedents" in non-negative.
+all(data$total_decedents > 0)
+
+
